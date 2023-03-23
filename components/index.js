55cb5ca0bfffc59061c86f4d6649036ac0d16941
Asjal Abrar <asjalabrar8@gmail.com>
2023-03-24 00:27:48 +0500

    Starter Template

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import Categories from "./Categories";
+import PostWidget from "./PostWidget";
+import PostsCard from "./PostsCard";
+
+export { Header, Categories, PostWidget, PostsCard };
